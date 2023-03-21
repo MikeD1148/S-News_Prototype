670be030ad6715b8fa01a2edd7f31571440fb279
Michael Watling <c0037932@my.shu.ac.uk>
2023-03-21 21:00:15 +0000

    Added an options menu with accessibility settings and user preferences. Can Toggle between Light and Dark Mode. Improved layout design.

--- app/src/main/java/com/example/s_newsprototype2/MainActivity.java
+++ app/src/main/java/com/example/s_newsprototype2/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.s_newsprototype2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
 import android.widget.Button;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
@@ -19,6 +30,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,14 +38,61 @@ public class MainActivity extends AppCompatActivity {
     LinearLayoutManager layoutManager;
     List<ViewStructure> articleList;
     Adapter adapter;
+    boolean darkMode;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Set Action Bar Colour
+        Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.grey, getTheme())));
+
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        darkMode = sharedPreferences.getBoolean("dark", false);
+
+        if (darkMode){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
         initData();
         initRecyclerView();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.preference1:
+                if (darkMode) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("dark", false);
+                }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("dark", true);
+                }
+                editor.apply();
+                Toast.makeText(this, "Theme Changed", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.preference2:
+                Toast.makeText(this, "Article Preferences", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
     }
 
